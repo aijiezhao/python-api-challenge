9f8a6ffb252be84d0ff9bebaaf6bc913cbc74c48
Aijie Zhao <aijie@uoregon.edu>
2020-08-05 18:06:15 -0700

    final

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,2 @@
+# Google API Key
+g_key = "AIzaSyCoDQ6kA525OYIWq52zwc7Q3y8QCg3NuYU"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "5eb4e218b6d216d7449d00e7b1062c0c"
+
+# Google API Key
+g_key = "AIzaSyCoDQ6kA525OYIWq52zwc7Q3y8QCg3NuYU"
